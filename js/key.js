6e6ed8ad65001453849a6465aac2229322488bef
unknown <johc0205@naver.com>
2022-01-09 19:37:41 +0900

    vanila js 수강 완료

---
+++ js/key.js
@@ -0,0 +1,3 @@
+function key(){
+    return "8c7115420623ec9f25dbe6fdbb9fd1fb";
+}
--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,20 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
 const API_KEY = "8c7115420623ec9f25dbe6fdbb9fd1fb"
 
-function onGeoOk(position){
+function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
-    .then((response) => response.json())
-    .then((data)=>{
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
-        city.innerText = data.name
+      .then((response) => response.json())
+      .then((data) => {
+        city.innerText = data.name;
         weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-    });
-}
-
-function onGeoError(){
+      });
+  }
+  function onGeoError() {
     alert("Can't find you. No weather for you.");
-}
-
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+  }
+  
+  navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
